c7f1cac6468751e3cde6fb5d36d1a4924126a9d0
TechnoStudy <info@techno.study>
2020-03-03 20:56:50 -0500

    first test

--- src/test/java/AlertTest.java
+++ src/test/java/AlertTest.java
@@ -1,8 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class AlertTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void init(){
+        System.setProperty("webdriver.chrome.driver", "D:\\TechnoStudy\\Selenium\\ChromeDriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get( "https://www.seleniumeasy.com/test/javascript-alert-box-demo.html" );
+    }
     @Test
     public void simpleAlert(){
+        String expectedText = "I am an alert box!";
+        driver.findElement( By.cssSelector( "button[onclick=\"myAlertFunction()\"]" ) ).click();
+        String actualText = driver.switchTo().alert().getText();
+        Assert.assertEquals( actualText, expectedText );
 
+        driver.switchTo().alert().dismiss();
+    }
+
+    @Test
+    public void confirmAlert(){
+
+    }
+    @AfterClass
+    public void quit(){
+        driver.quit();
     }
 }
